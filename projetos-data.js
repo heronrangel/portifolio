dc306234a1a67d1a01b8c3b5e3c4f73f9b1cfd13
Heron Rangel Agostinho <heronlennon@gmail.com>
2025-11-27 14:15:17 -0300

    Update projetos-data.js

--- projetos-data.js
+++ projetos-data.js
@@ -24,10 +24,11 @@ const projetos = {
       { src: net_icon, alt: ".NET MAUI" },
       { src: xamarin_icon, alt: "Xamarin Forms" },
       { src: sqlite_icon, alt: "SQLite" },
+      { src: json_icon, alt: "JSON" },
       { src: android_icon, alt: "Android" },
       { src: ios_icon, alt: "iOS" }
     ],
-    descricaoCurta: 'App mobile para gestão de pedidos e orçamentos.',
+    descricaoCurta: 'App mobile para gestão de pedidos e orçamentos',
     descricaoCompleta: 'App mobile desenvolvido para a força de vendas, permitindo a gestão completa de clientes, cadastro de produtos, e emissão de pedidos e orçamentos online e offline. A sincronização de dados é feita via API e atualmente conta com mais de 250 usuários ativos.',
     imagens: [
       { src: "./assets/images/projetos/control_master.png", alt: "Demonstração do projeto" }
@@ -41,10 +42,11 @@ const projetos = {
       { src: net_icon, alt: ".NET MAUI" },
       { src: xamarin_icon, alt: "Xamarin Forms" },
       { src: sqlite_icon, alt: "SQLite" },
+      { src: json_icon, alt: "JSON" },
       { src: android_icon, alt: "Android" },
       { src: ios_icon, alt: "iOS" }
     ],
-    descricaoCurta: 'App mobile para montagem de cortinas e persianas.',
+    descricaoCurta: 'App mobile para montagem de cortinas e persianas',
     descricaoCompleta: 'App mobile desenvolvido para auxiliar revendas na montagem de cortinas e persianas. O produto possui uma estrutura totalmente dinâmica, permitindo configurar medidas, sobras de tecido, cortes e valores personalizados para cada item da lista. Ao final, a cortina ou persiana é consolidada em um único produto, enviado diretamente ao sistema da fábrica já pronto para produção. Atualmente, o app atende duas revendas ativas, cada uma com mais de 120 usuários.',
     imagens: [
       { src: "./assets/images/projetos/mobile_persianas.png", alt: "Demonstração do projeto" }
@@ -57,10 +59,11 @@ const projetos = {
     tecnologias: [
       { src: csharp_icon, alt: "C#" },
       { src: net_icon, alt: ".NET Framework" },
+      { src: json_icon, alt: "JSON" },
       { src: access_icon, alt: "Access" }
     ],
-    descricaoCurta: 'Software de geração de certificados para APIs bancárias.',
-    descricaoCompleta: 'Ferramenta Windows para geração de certificados de APIs bancárias (boletos e PIX) integrada com banco de dados Access. Com a diversidade de tipos de certificados exigidos pelos bancos e as diferentes maneiras de gerá-los via OpenSSL, GitBash e outras ferramentas, este software reúne todo o processo em uma única interface simples e intuitiva. Ele padroniza a criação dos certificados e entrega o resultado final em um arquivo PFX, garantindo praticidade, consistência e redução de erros.',
+    descricaoCurta: 'Software para gerar certificados de APIs bancárias',
+    descricaoCompleta: 'Ferramenta Windows para gerar certificados de APIs bancárias (boletos e PIX) integrada com banco de dados Access. Com a diversidade de tipos de certificados exigidos pelos bancos e as diferentes maneiras de gerá-los via OpenSSL, GitBash e outras ferramentas, esse software reúne todo o processo em uma única interface simples e intuitiva. Ele padroniza a criação dos certificados e entrega o resultado final em um arquivo PFX, garantindo praticidade, consistência e redução de erros.',
     imagens: [
       { src: "./assets/images/projetos/conversor_certificados.png", alt: "Demonstração do projeto" }
     ]
@@ -72,10 +75,11 @@ const projetos = {
       { src: vba_icon, alt: "VBA" },
       { src: access_icon, alt: "Access" },
       { src: woo_icon, alt: "WooCommerce" },
+      { src: json_icon, alt: "JSON" },
       { src: wordpress_icon, alt: "WordPress" }
     ],
-    descricaoCurta: 'Software integrador de lojas e-commerce.',
-    descricaoCompleta: 'Solução de integração entre ERP e plataformas de e-commerce. Desenvolvida em VBA Access, esta solução conecta o sistema interno de gestão a lojas virtuais como WooCommerce, Bagy e NuvemShop. A integração via API inclui a atualização de preços, estoque e importação de pedidos, garantindo praticidade e a redução de retrabalho.',
+    descricaoCurta: 'Software integrador de lojas e-commerce',
+    descricaoCompleta: 'Solução de integração entre ERP e plataformas de e-commerce. Desenvolvida em VBA Access, essa solução conecta o sistema interno de gestão a lojas virtuais como WooCommerce, Bagy e NuvemShop. A integração via API inclui atualização de preços, estoque e importação de pedidos, garantindo praticidade e a redução de retrabalho.',
     imagens: [
       { src: "./assets/images/projetos/loja_virtual.png", alt: "Demonstração do projeto" }
     ]
@@ -89,22 +93,22 @@ const projetos = {
       { src: vba_icon, alt: "VBA" },
       { src: access_icon, alt: "Access" }
     ],
-    descricaoCurta: 'API REST para banco de dados Access.',
+    descricaoCurta: 'API REST para banco de dados Access',
     descricaoCompleta: 'Desenvolvimento de uma API RESTful para servir como ponte entre banco de dados Access e diversas aplicações.',
     imagens: [
       { src: "./assets/images/projetos/api_vba.png", alt: "Diagrama da arquitetura da API" }
     ]
   },
     '6': {
-    titulo: 'Software de backup',
+    titulo: 'Software de Backup',
     categoria: 'desktop',
     tecnologias: [
       { src: csharp_icon, alt: "C#" },
       { src: access_icon, alt: "Access" },
       { src: net_icon, alt: ".NET Framework" }
     ],
-    descricaoCurta: 'Software de backup FTP.',
-    descricaoCompleta: 'Este software lê um arquivo XML contendo uma fila de arquivos a serem compactados e enviados como backup. Após o processamento, os arquivos são transmitidos via FTP para o servidor do sistema ERP.',
+    descricaoCurta: 'Software de backup FTP',
+    descricaoCompleta: 'Esse software lê um arquivo XML contendo uma fila de arquivos a serem compactados e enviados como backup. Após o processamento, os arquivos são transmitidos via FTP para o servidor do sistema ERP.',
     imagens: [
       { src: "./assets/images/projetos/backup.png", alt: "Demonstração do projeto" }
     ]
@@ -117,8 +121,8 @@ const projetos = {
       { src: access_icon, alt: "Access" },
       { src: net_icon, alt: ".NET Framework" }
     ],
-    descricaoCurta: 'Instalador de sistema ERP.',
-    descricaoCompleta: 'Este software centraliza toda a estrutura necessária para a instalação do sistema ERP, incluindo o registro de DLLs e demais utilitários essenciais. Além disso, oferece recursos para migração e atualização completa do sistema.',
+    descricaoCurta: 'Instalador de sistema ERP',
+    descricaoCompleta: 'Esse software centraliza toda a estrutura necessária para a instalação do sistema ERP, incluindo o registro de DLLs e demais utilitários essenciais. Além disso, oferece recursos para migração e atualização completa do sistema.',
     imagens: [
       { src: "./assets/images/projetos/instalador_sisop.png", alt: "Demonstração do projeto" }
     ]
@@ -130,8 +134,8 @@ const projetos = {
       { src: csharp_icon, alt: "C#" },
       { src: net_icon, alt: ".NET Framework" }
     ],
-    descricaoCurta: 'Software gerenciador de backups de sistema ERP.',
-    descricaoCompleta: 'Este software monitora, a cada hora, a pasta local de backups, evitando a ocupação excessiva do disco rígido. Ele mantém apenas uma quantidade definida de arquivos, garantindo que sempre permaneçam armazenados os backups mais recentes.',
+    descricaoCurta: 'Software gerenciador de backups de sistema ERP',
+    descricaoCompleta: 'Esse software monitora, de hora em hora, a pasta local de backups, evitando a ocupação excessiva do disco rígido, mantendo apenas uma quantidade definida de arquivos e garantindo que sempre permaneçam armazenados os backups mais recentes.',
     imagens: [
       { src: "./assets/images/projetos/organiza_backup.png", alt: "Demonstração do projeto" }
     ]
@@ -143,8 +147,8 @@ const projetos = {
       { src: csharp_icon, alt: "C#" },
       { src: net_icon, alt: ".NET Framework" }
     ],
-    descricaoCurta: 'Software Instalador de API IIS.',
-    descricaoCompleta: 'Este software instala uma API localmente no IIS, utilizando o IP e a porta definidos durante o processo de instalação. Todos os recursos necessários são baixados via FTP e instalados automáticamente. Também oferece recursos de gerenciamento da aplicação, permitindo iniciar, parar e realizar testes de funcionamento. Além disso, possibilita atualizar a API para versões mais recentes.',
+    descricaoCurta: 'Software Instalador de API IIS',
+    descricaoCompleta: 'Esse software instala uma API localmente no IIS, utilizando o IP e a porta definidos durante o processo de instalação. Todos os recursos necessários são baixados via FTP e instalados automáticamente. Também oferece recursos de gerenciamento da aplicação, permitindo iniciar, parar e realizar testes de funcionamento. Além disso, possibilita atualizar a API para versões mais recentes.',
     imagens: [
       { src: "./assets/images/projetos/instalador_api.png", alt: "Demonstração do projeto" }
     ]
@@ -154,10 +158,11 @@ const projetos = {
     categoria: 'desktop',
     tecnologias: [
       { src: csharp_icon, alt: "C#" },
-      { src: net_icon, alt: ".NET Framework" }
+      { src: net_icon, alt: ".NET Framework" },
+      { src: json_icon, alt: "JSON" }
     ],
-    descricaoCurta: 'Gerenciador SFTP.',
-    descricaoCompleta: 'Este software facilita o gerenciamento de arquivos e versões de um sistema ERP. Com ele, é possível criar uma lista de processos que devem ser executados, como o envio de arquivos para um servidor SFTP. Também é possível definir se o arquivo será compactado antes do envio ou enviado em sua forma original.',
+    descricaoCurta: 'Gerenciador SFTP',
+    descricaoCompleta: 'Esse software facilita o gerenciamento de arquivos e versões de um sistema ERP, sendo possível criar uma lista de processos que devem ser executados, como o envio de arquivos para um servidor SFTP, bem como, é possível definir se o arquivo será compactado antes do envio ou enviado em seu formato original.',
     imagens: [
       { src: "./assets/images/projetos/gerenciador_sftp.png", alt: "Demonstração do projeto" }
     ]
@@ -169,7 +174,7 @@ const projetos = {
       { src: wordpress_icon, alt: "WordPress" },
       { src: php_icon, alt: "PHP" }
     ],
-    descricaoCurta: 'Site institucional SysOpRS.',
+    descricaoCurta: 'Site institucional SysOpRS',
     descricaoCompleta: 'O site institucional da SysOpRS apresenta o sistema de gestão SisOp, destacando suas funcionalidades, benefícios e soluções para empresas. A plataforma combina facilidade de navegação, design responsivo e integração de recursos personalizados, oferecendo uma experiência completa aos visitantes.',
     imagens: [
       { src: "./assets/images/projetos/site_sisop.png", alt: "Demonstração do projeto" }
@@ -184,7 +189,7 @@ const projetos = {
       { src: php_icon, alt: "PHP" },
       { src: js_icon, alt: "JavaScript" }
     ],
-    descricaoCurta: 'Site institucional Seven Sports.',
+    descricaoCurta: 'Site institucional Seven Sports',
     descricaoCompleta: 'O site institucional da Seven Sports permite a exibição detalhada dos atletas, organizados por nacionalidade, posição, altura e peso. Além da apresentação pública, conta com um painel interno intuitivo e dinâmico, que possibilita aos moderadores cadastrar e gerenciar os atletas de forma prática, garantindo atualização constante das informações.',
     imagens: [
       { src: "./assets/images/projetos/site_seven.png", alt: "Demonstração do projeto" }
@@ -197,9 +202,10 @@ const projetos = {
     tecnologias: [
       { src: wordpress_icon, alt: "WordPress" },
       { src: php_icon, alt: "PHP" },
-      { src: js_icon, alt: "JavaScript" }
+      { src: js_icon, alt: "JavaScript" },
+      { src: json_icon, alt: "JSON" }
     ],
-    descricaoCurta: 'Site Central de Ajuda SysOpRS.',
+    descricaoCurta: 'Site Central de Ajuda',
     descricaoCompleta: 'O site da Central de Ajuda do SisOp reúne mais de 550 manuais e tutoriais sobre os diversos módulos do sistema, oferecendo suporte completo aos usuários. Além da seção pública de conteúdo, conta com um painel de moderação totalmente personalizado, que permite a criação de novos manuais, atualização de conteúdos existentes e gerenciamento de contatos, garantindo um suporte ágil e organizado.',
     imagens: [
       { src: "./assets/images/projetos/site_central_ajuda.png", alt: "Demonstração do projeto" }
@@ -213,7 +219,7 @@ const projetos = {
       { src: wordpress_icon, alt: "WordPress" },
       { src: php_icon, alt: "PHP" }
     ],
-    descricaoCurta: 'Site casa de festas HALL 239.',
+    descricaoCurta: 'Site casa de festas HALL 239',
     descricaoCompleta: 'Site institucional para a casa de festas HALL 239, destacando o ambiente, serviços e facilidade de agendamento de visitas/orçamentos com galeria de fotos integrada.',
     imagens: [
       { src: "./assets/images/projetos/site_hall.png", alt: "Demonstração do projeto" }
@@ -226,9 +232,10 @@ const projetos = {
     tecnologias: [
       { src: wordpress_icon, alt: "WordPress" },
       { src: php_icon, alt: "PHP" },
-      { src: js_icon, alt: "JavaScript" }
+      { src: js_icon, alt: "JavaScript" },
+      { src: json_icon, alt: "JSON" }
     ],
-    descricaoCurta: 'Site WL-TecField.',
+    descricaoCurta: 'Site WL-TecField',
     descricaoCompleta: 'O site da WL-TecField apresenta os equipamentos de caixa de auto-atendimento oferecidos pela empresa, destacando suas funcionalidades e diferenciais. Além disso, a plataforma conta com uma seção dedicada ao recrutamento de técnicos em todo o Brasil, facilitando a candidatura e gestão de novos profissionais.',
     imagens: [
       { src: "./assets/images/projetos/site_wl.png", alt: "Demonstração do projeto" }
